ce32b502629c31a403110618e880956dc91317c5
filippo-falcone <filippofalcone98@hotmail.com>
2026-04-01 21:00:06 +0200

    updat:  aggiunta la docstring per la funzione di parsing dei file di testo

--- src/tex_converter/parsers/txt_parser.py
+++ src/tex_converter/parsers/txt_parser.py
@@ -4,6 +4,13 @@ from ..model.blocks import Paragraph
 
 
 def parse_txt(path: str) -> Document:
+    """Funzione per analizzare un file di testo e convertirlo in un documento LaTeX.
+    Ogni riga del file di testo viene considerata un paragrafo separato.
+    Args:
+        path (str): Il percorso del file di testo da analizzare.
+    Returns:
+        Document: Il documento LaTeX.
+    """
     blocks: List[Block] = []
 
     with open(path, "r") as f:
