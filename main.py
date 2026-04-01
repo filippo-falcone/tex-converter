9a156cb6539adefe9235f3727d3aa96fcb8c992e
filippo-falcone <filippofalcone98@hotmail.com>
2026-04-01 22:01:47 +0200

    feat: aggiornato Main per accettare input e output da argomenti della riga di comando utilizzando sys

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import sys
 from src.tex_converter.converter import Convert
 
 
 def Main() -> None:
-    InputPath: str = "sample.txt"
-    Outpath: str = "output.tex"
+    if len(sys.argv) < 3:
+        print("Uso corretto: python main.py <input_file> <output_file>")
+        return
+    InputPath: str = sys.argv[1]
+    Outpath: str = sys.argv[2]
     print(f"Converto {InputPath} in {Outpath} ... ")
     Convert(InputPath, Outpath)
     print("Conversione completata!")
