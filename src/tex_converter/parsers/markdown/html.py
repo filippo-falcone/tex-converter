3a0b43ea72cad88d87c63b49b83f1bf18a5b5735
filippo-falcone <filippofalcone98@hotmail.com>
2026-04-02 23:13:53 +0200

    refactor: rimosso codice obsoleto e file non più utilizzati nel parser Markdown

--- src/tex_converter/parsers/markdown/html.py
+++
@@ -1,9 +0,0 @@
-# ============================================================
-# HTML TAGS PARSER
-# ============================================================
-# This module will handle HTML-specific markdown extensions:
-# - Superscript (<sup>text</sup>)
-# - Subscript (<sub>text</sub>)
-# - Highlight (<mark>text</mark>)
-#
-# Currently empty - will be implemented in Phase 2
